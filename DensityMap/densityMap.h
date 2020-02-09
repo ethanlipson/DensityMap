19b23910213a969602b0cd193dd70a32e8a8adc9
Ethan Lipson <ethan.lipson@gmail.com>
2020-02-08 23:00:05 -0500

    Removed DensityMap::addLineSmoothed()

--- DensityMap/densityMap.h
+++ DensityMap/densityMap.h
@@ -19,19 +19,6 @@ public:
 	DensityMap(int dim);
 
 	// Adds a line of data between p1 and p2
-	// The area around the line is faded
-	// -----
-	// I do not recommend using this if you have a lot of data
-	// because the result will look blurry
-	// (like with ultrasound data !!!)
-	void addLineSmoothed(glm::vec3 p1, glm::vec3 p2, std::vector<unsigned char> vals, int radius = 5);
-
-	// Adds a line of data between p1 and p2
-	// The line is not smoothed with the surrounding area
-	// -----
-	// I recommend using this if you have a lot of data
-	// because if you use DensityMap::addLineSmoothed()
-	// then the result will look blurry
 	void addLine(glm::vec3 p1, glm::vec3 p2, std::vector<unsigned char> vals);
 
 	// Overwrites everything with value
